134bcc3119e1cf4e55b7c72e215f28351d4b2087
Parviz Azimov <parviz.azimov@student.lamk.fi>
2022-05-21 15:28:06 +0300

    initial
    Signed-off-by: Parviz Azimov <parviz.azimov@student.lamk.fi>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/todolist.js";
+import Modal from "./components/modal.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: ["Finish homework", "Wash dishes", "Clean room", "Make waffles"],
+        };
+    }
+
+    addTodo = todo => {
+        this.setState(prevState => ({
+            todos: [...prevState.todos, todo],
+        }));
+    };
+    
+    render() {
+        return(
+            <div className="App">
+              <h1>Todos</h1>
+              <TodoList todos={this.state.todos}/>
+              <Modal addTodo={this.addTodo}/>
+            </div>
+        );
+    }
 }
-
 export default App;
