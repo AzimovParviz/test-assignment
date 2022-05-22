3f8e23e3239f69481474c53139363de27364d79b
Parviz Azimov <parviz.azimov@student.lamk.fi>
2022-05-22 17:46:28 +0300

    ability to add new todo items, but no ability to edit them. minor changes to layout
    Signed-off-by: Parviz Azimov <parviz.azimov@student.lamk.fi>

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Modal, Button} from 'react-bootstrap';
 
 const TodoForm = () => {
-    const [todos, addTodos] = useState([{title: '',status:'', deadline:''}]);
+    const [todos, addTodos] = useState([{id: 0, title: '',status:'', deadline:''}]);
     const [status, setStatus] = useState("In Progress");
     const [deadline, setDeadline] = useState("");
     const [todoInput, setToDoInput]  = useState("");
@@ -13,29 +13,81 @@ const TodoForm = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     
-    async function getTodo() {
-    }
+    const [showEdit, setShowEdit] = useState(false);
+    const handleCloseEdit = () => setShowEdit(false);
+    const handleShowEdit = () => setShowEdit(true);
     
     const handleSubmit = (event) => {
         event.preventDefault();
         addTodos(todos.concat(
             {
+                id: todos[todos.length-1].id+=1,
                 title: todoInput,
                 status: status,
                 deadline: deadline
             }
         ));
         setToDoInput("");
-        console.log(todoInput);
+        setDeadline("");
+        console.log(todos);
     };
 
+    const handleEditSubmit = (event) => {
+        event.preventDefault();
+        addTodos(todos.concat(
+            {
+                id: todos[todos.length-1].id+=1,
+                title: todoInput,
+                status: status,
+                deadline: deadline
+            }
+        ));
+        setToDoInput("");
+        setDeadline("");
+        console.log(todos);
+    };
+
+    
     const handleStatusChange = (event) => {
         setStatus(event.target.value);
     };
     
     return (
         <div>
-          <TodoList todos={todos}/>
+          <ul>
+            <div onDoubleClick={handleShowEdit}>            
+              <TodoList todos={todos}/>
+            </div>
+          </ul>
+          <Modal show={showEdit} onHide={handleCloseEdit}>
+            <Modal.Header>
+              Enter information
+            </Modal.Header>
+            <form onSubmit={handleEditSubmit}>
+              <input
+                id="todo-input"
+                type="text"
+                placeholder="Add todo"
+                value={todos.title}
+                onChange={event => setToDoInput(event.target.value)}
+              />
+            <br/>
+            <input
+              id="deadline-input"
+              type="text"
+              placeholder="set deadline"
+        value={todos.deadline}
+              onChange={e => setDeadline(e.target.value)}
+            />
+              <br/>
+              <select id="status-input" value={todos[0].status} onChange={handleStatusChange}>
+                <option value="Done">Done</option>
+                <option value="Not Started">Not Started</option>
+                <option value="In Progress">In progress</option>
+              </select>
+              <input type="submit" value="Submit"/>
+            </form>
+          </Modal>
           <>
           <Button onClick={handleShow}>New Todo</Button>
           <Modal show={show} onHide={handleClose}>
@@ -61,15 +113,16 @@ const TodoForm = () => {
               <br/>
               <select id="status-input" value={status} onChange={handleStatusChange}>
                 <option value="Done">Done</option>
-              <option value="Not Started">Not Started</option>
+                <option value="Not Started">Not Started</option>
                 <option value="In Progress">In progress</option>
               </select>
               <input type="submit" value="Submit"/>
-            </form>            
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
+            </form>
+            <Modal.Footer>
+            {/*<Button variant="primary" onClick={handleClose}>
+               Save Changes
+               </Button>*/}
+              Click anywhere outside to close this popup
           </Modal.Footer>
         </Modal>
           </>
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-const Todo = props => <li>{props.todo} <br/> status: {props.status} <br/> deadline: {props.deadline}</li>;
-const TodoList = props => (
-  <ul>
-    {props.todos.map(todo => (
-	<Todo todo={todo.title} key={todo.title} status={todo.status} deadline={todo.deadline}/>
-    ))}
-  </ul>
-);
+const TodoList = props => {        
+    const Todo = props => <li>
+                              title: {props.todo} <br/> status: {props.status} <br/> deadline: {props.deadline}
+                          </li>;
+    
+    return(
+        <>
+          {props.todos.map(todo => (
+	      <Todo todo={todo.title} key={todo.title} status={todo.status} deadline={todo.deadline}/>
+          ))}
+        </>
+    );
+};
 
 export default TodoList;
